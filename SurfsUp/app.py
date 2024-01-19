cfafa7dcded05db360e544c53ddb23825a49e472
TRegan5 <tpregan5@gmail.com>
2024-01-18 23:36:24 -0500

    Continuing to add code for queries to be used by various routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -36,6 +36,19 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+# Find the most recent date in the data set.
+last_date = session.query(measurements.date).order_by(measurements.date.desc()).first()#[0]
+print(last_date)
+
+# Calculate the date one year from the last date in data set.
+last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+print(last_year)
+
+# calculate the lowest, highest, and average temperature
+most_active_stats = session.query(func.min(measurements.tobs), func.max(measurements.tobs), func.avg(measurements.tobs)).\
+    filter(measurements.station == most_active).all()
+
 # Climate App Design 1
 @app.route("/")
 def home():
@@ -54,18 +67,20 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def home():
     print("Server received request for 'Precipitation' page...")
-    return "Welcome to my 'Precipitation' page!"
+    last_yr_prcp = session.query(measurements.date, measurements.prcp).\
+        filter(measurements.date >= last_year).\
+            filter(measurements.prcp != None).\
+            order_by(measurements.date).all()
+    return jsonify(last_yr_prcp)
 
 # Climate App Design 3
 @app.route("/api/v1.0/stations")
 def home():
+    print("Server received request for 'Stations' page...")
     # Query list of stations
     query = session.query(stations.station).all()
     station_list = list(np.ravel(query))
-    print("Server received request for 'Stations' page...")
-    return (
-        f"Welcome to my 'Stations' page!<br/>"
-        jsonify(station_list))
+    return jsonify(station_list)
 
 # Climate App Design 4
 @app.route("/api/v1.0/tobs")
